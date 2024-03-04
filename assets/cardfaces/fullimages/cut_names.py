cf18529388cb8f5cc70679b4fbacf3b1265960f6
Vladimir Jovanovic <64062930+vlad-jovanovic@users.noreply.github.com>
2024-03-03 17:59:51 -0800

    [assets/tarot_*.json] Updates on the JSON structure for tarot deck and prompts and processing

--- assets/cardfaces/fullimages/cut_names.py
+++ assets/cardfaces/fullimages/cut_names.py
@@ -3,31 +3,23 @@ from pathlib import Path
 import os
 
 # Read in the JSON
-tarot_json_path = Path("./assets/tarot-deck-setup.json")
+tarot_json_path = Path("./assets/tarot_deck_numbered.json")
 with open(tarot_json_path, 'r') as json_file:
-    deck_dict = json.load(json_file)
-# Flatten out the list
-flat_deck = deck_dict["MajorArcana"]+\
-    deck_dict["MinorArcana"]["Wands"]+\
-    deck_dict["MinorArcana"]["Cups"]+\
-    deck_dict["MinorArcana"]["Swords"]+\
-    deck_dict["MinorArcana"]["Pentacles"]
-print(flat_deck)
-flat_deck = [(f"{i:02d}",card) for (i,card) in zip(range(1,79),flat_deck)]
-print(flat_deck)
+    deck_array = json.load(json_file, parse_int=lambda id: int(id) if id.isnumeric() else id)
+print(deck_array)
 
 # Find the JPEGs
 full_image_path = Path("./assets/cardfaces/fullimages")
 tarot_jpgs = sorted(full_image_path.glob("*.jpg"))
 
 # For each of the files, also look at the supposed corresponding ID
-for (card_file,(id,name)) in zip(tarot_jpgs,flat_deck):
+for (card_file,card) in zip(tarot_jpgs,deck_array):
     card_stem = card_file.stem
     found_id = card_stem.split("_")[1]
     # Each file has numbers_id_numbers.jpg
-    if id == found_id:
+    if card["ordering"]+1 == int(found_id):
         # Have a match on the cards and can rename
-        new_card = Path(card_file.parent,f'{id}_{name.replace(" ","_")}.jpg')
+        new_card = Path(card_file.parent,f'{card["ordering"]}_{card["name"].replace(" ","_")}.jpg')
         print(f'Renaming: {card_file} to {new_card}')
         os.rename(card_file,new_card)
     else:
